cbac218f2029e2bb2b548c693415a125581d6f65
Farzan <farzan.hosseiny@gmail.com>
2023-06-23 17:34:02 +0330

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='flex flex-col w-full h-screen'>
+
+      <div className='h-20 w-full'>
+         this is for header 
+      </div>
+      <div className='grid grid-cols-12'>
+        this is our mainscreen
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
