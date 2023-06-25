791298292fb61bd1a5619fff5a721d8530d70a45
Farzan <farzan.hosseiny@gmail.com>
2023-06-25 11:36:10 +0330

    layout completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { CiSearch } from "react-icons/ci";
 import { HiChevronDown } from 'react-icons/hi'
 const HomePage = () => {
   return (
-    <div className="flex h-screen w-full flex-col">
+    <div className="flex h-full w-full flex-col">
       <header className="flex h-20 w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white">
         <div>
           <IoReorderThreeOutline className="text-2xl text-gray-600" />
@@ -29,7 +29,7 @@ const HomePage = () => {
           </div>
         </div>
       </header>
-      <section className="grid h-full w-full grid-cols-12 place-items-center">
+      <section className="grid  grid-cols-12 ">
         <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
           <div className="flex w-full flex-col space-y-4 py-10 ">
             <div className="flex w-full items-center space-x-4">
@@ -79,7 +79,7 @@ const HomePage = () => {
                 className="group flex flex-col space-y-4 border-b border-gray-300 pb-8 last:border-none"
               >
                 <div className="flex w-full items-center space-x-2">
-                  <div className="h-4 w-5 rounded-full bg-gray-400"></div>
+                  <div className="h-10 w-10 rounded-full bg-gray-400"></div>
                   <div>
                     <p className="font-semibold">
                       Farzan Hosseini &#x2022; 22 Dec. 2022
@@ -93,7 +93,7 @@ const HomePage = () => {
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Dolores, odio!
                     </p>
-                    <p className="text-sm text-gray-500 break-words">
+                    <p className="break-words text-sm text-gray-500">
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                       Nobis rerum earum nisi corporis qui cum quis eveniet
                       dolores facilis obcaecati! Dolorum velit, error neque,
@@ -127,7 +127,58 @@ const HomePage = () => {
             ))}
           </div>
         </main>
-        <aside className="col-span-4 h-full w-full"></aside>
+        <aside className="col-span-4  w-full space-y-4 p-6">
+          <div>
+            <h3 className="my-6 text-lg font-semibold">
+              Pepople you might be interessted
+            </h3>
+            <div className="flex flex-col space-y-4">
+              {Array.from({ length: 4 }).map((_, i) => (
+                <div key={i} className="flex flex-row items-center space-x-5">
+                  <div className="h-10 w-10 flex-none rounded-full bg-gray-300"></div>
+                  <div>
+                    <div className="text-sm font-bold text-gray-900">
+                      John Doe
+                    </div>
+                    <div className="text-xs">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Eius nostrum recusandae tenetur perferendis?
+                    </div>
+                  </div>
+                  <div>
+                    <button className="flex items-center space-x-3 rounded border border-gray-400 px-4  py-2.5 transition hover:border-gray-900 hover:text-gray-900">
+                      Follow
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="sticky top-20 ">
+            <h3 className="my-6 text-lg font-semibold">Your reading list</h3>
+            <div className="flex flex-col space-y-8">
+              {Array.from({ length: 4 }).map((_, i) => (
+                <div key={i} className="group flex items-center space-x-6">
+                  <div className="aspect-square h-full w-2/5 rounded-xl bg-gray-300"></div>
+                  <div className="flex w-3/5 flex-col space-y-2">
+                    <div className="text-lg font-semibold decoration-indigo-600 group-hover:underline">
+                      Lorem ipsum dolor sit amet consectetur.
+                    </div>
+                    <div>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Delectus alias voluptatibus nulla possimus quod?
+                    </div>
+                    <div className="flex w-full items-center space-x-4">
+                      <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+                      <div> Farzan Hosseini &#x2022; </div>
+                      <div>22 Dec. 2022</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </aside>
       </section>
     </div>
   );
