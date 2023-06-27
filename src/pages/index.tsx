8c54c7ba66cfb6a4173e1f659ce5091e8690529d
Farzan <farzan.hosseiny@gmail.com>
2023-06-27 08:10:10 +0330

    layout added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,12 @@
 import React from "react";
-import { IoReorderThreeOutline } from "react-icons/io5";
-import { BsBell } from "react-icons/bs";
-import { FiEdit } from "react-icons/fi";
 import { CiSearch } from "react-icons/ci";
 import { HiChevronDown } from 'react-icons/hi'
+import MainLayout from "../layouts/MainLayout";
+
 const HomePage = () => {
   return (
-    <div className="flex h-full w-full flex-col">
-      <header className="flex h-20 w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white">
-        <div>
-          <IoReorderThreeOutline className="text-2xl text-gray-600" />
-        </div>
-        <div className="text-xl font-thin">Ultimate Blog App</div>
-        <div className="flex items-center space-x-4">
-          <div>
-            <BsBell className="text-2xl text-gray-600" />
-          </div>
-          <div>
-            <div className="h-5 w-5 rounded-full bg-gray-600" />
-          </div>
-          <div>
-            <button className="flex items-center space-x-3 rounded border border-gray-200 px-4  py-2.5 transition hover:border-gray-900 hover:text-gray-900">
-              <div>Write</div>
-              <div>
-                <FiEdit />
-              </div>
-            </button>
-          </div>
-        </div>
-      </header>
+    <MainLayout>
+
       <section className="grid  grid-cols-12 ">
         <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
           <div className="flex w-full flex-col space-y-4 py-10 ">
@@ -180,7 +158,7 @@ const HomePage = () => {
           </div>
         </aside>
       </section>
-    </div>
+    </MainLayout>
   );
 };
 
