5e253470946db1dcd9337aa38931c883c98091d0
Farzan <farzan.hosseiny@gmail.com>
2023-07-14 10:03:19 +0330

    trying to add vercel postgres to project

--- src/env/client.mjs
+++ src/env/client.mjs
@@ -1,7 +1,9 @@
 // @ts-check
-import { clientEnv, clientSchema } from "./schema.mjs";
+// import { clientEnv, clientSchema } from "./schema.mjs";
+import { PrismaClient } from '@prisma/client';
 
-const _clientEnv = clientSchema.safeParse(clientEnv);
+// const _clientEnv = clientSchema.safeParse(clientEnv);
+const prisma = new PrismaClient();
 
 export const formatErrors = (
   /** @type {import('zod').ZodFormattedError<Map<string,string>,string>} */
@@ -14,22 +16,10 @@ export const formatErrors = (
     })
     .filter(Boolean);
 
-if (!_clientEnv.success) {
+if (!prisma) {
   console.error(
     "❌ Invalid environment variables:\n",
-    ...formatErrors(_clientEnv.error.format()),
   );
   throw new Error("Invalid environment variables");
 }
 
-for (let key of Object.keys(_clientEnv.data)) {
-  if (!key.startsWith("NEXT_PUBLIC_")) {
-    console.warn(
-      `❌ Invalid public environment variable name: ${key}. It must begin with 'NEXT_PUBLIC_'`,
-    );
-
-    throw new Error("Invalid public environment variable name");
-  }
-}
-
-export const env = _clientEnv.data;
